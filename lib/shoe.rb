26e1b75a3e7a9a09c411e63dc48e9e1be90cd8c8
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-12 01:22:34 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :properies, :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+  
+  def cobble=(repaired)
+    @cobble = repaired
+  end
+  
+  def cobble
+    @cobble
+    puts "Your shoe is as good as new!"
+  end
+  
+end
